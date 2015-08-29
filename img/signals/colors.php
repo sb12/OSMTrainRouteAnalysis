23a5adf2cead4b9b59549050ce4d2d65d1aa672f
PeterDRS <ph@ersatzhalt.de>
2015-08-30 00:12:41 +0200

    Improve color values

--- img/signals/colors.php
+++ img/signals/colors.php
@@ -1,7 +1,8 @@
 <?php
 
-define("COLOR_RED", "#E02E45");
+define("COLOR_RED", "#F32124");
 define("COLOR_YELLOW", "#FF9714");
 define("COLOR_GREEN", "#38FFF5");
+define("COLOR_GRAY", "#232421")
 
 ?>
