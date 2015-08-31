e70cfb2032b86d0e5f805b2bc5c3c0afe3465afd
sb12 <steffen.bolender@gmail.com>
2015-08-31 20:15:20 +0200

    Overpass API status codes

--- functions/overpass.php
+++ functions/overpass.php
@@ -57,19 +57,24 @@ Class Overpass
 		// get data from overpass api
 		$content = @file_get_contents($link);
 		
+		// explanation about status codes of the overpass api can be found here: http://overpass-api.de/command_line.html
 		if( isset($http_response_header[0]) )
 		{
 			$status_code = self::getStatusCode($http_response_header[0]);
-			if ( $status_code != "200" )
+			if( $status_code != 200 ) // 200 OK -> everything is ok
 			{
-				if( $status_code == "400")
+				if( $status_code == 400 ) // 400 Bad Request -> syntax error
 				{
 					self::$error = Lang::l_("Invalid Overpass API query.");
 					//log error to send a correct query next time:
-					$msg = "Invalid Overpass API request. URI: " . $link;
+					$msg = "Invalid Overpass API request. Query: " . $query;
 					log_error($msg);
 				}
-				elseif( $status_code == "504" )
+				elseif( $status_code == 429 ) // 429 Too Many Requests -> too many requests from same ip
+				{
+					self::$error = Lang::l_("Too many requests to Overpass API at the same time.");
+				}
+				elseif( $status_code == 504 ) // 504 Gateway Timeout -> too much load on server
 				{
 					self::$error = Lang::l_("Overpass API currently overcrowded.");
 				}
@@ -77,7 +82,7 @@ Class Overpass
 				{
 					self::$error = Lang::l_("Unknown Error.");
 					//log error to show a proper error message next time:
-					$msg = "Unknown Error when requesting overpass api: " . $header . " | URI: " . $link;
+					$msg = "Unknown Error when requesting overpass api: " . $http_response_header[0] . " | URI: " . $link;
 					log_error($msg);
 				}
 				return false;
@@ -98,7 +103,13 @@ Class Overpass
 		self::$result = $content;
 		return true;
 	}
-	
+
+
+	/**
+	 * extracts the status code from the http_response_header
+	 * @param String $http_response_code
+	 * @return int status code
+	 */
 	static function getStatusCode($http_response_code)
 	{
 		$matches = array();
